e7a05d960114184ba4c0a5d44dd543dc8f33caf2
yokota kenji <mail@yokotakenji.me>
2017-12-08 20:08:19 +0900

    to more pliable

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,21 @@ const gulp     = require('gulp');
 const exec     = require('child_process').exec;
 const notifier = require('node-notifier');
 
-let input   = './main.cpp';
+let input   = './main.cpp ./sub.cpp';
 let output  = './dest/main';
-let compier = 'clang++';
 
-let frameworks = [
-    'GLUT',
-    'OpenGL'
-].map( v => `-framework ${v}` ).join(' ');
-
-let task = `${compier} ${frameworks} -o ${output} ${input}`;
+let command = [
+    'clang++',
+    '-std=c++11',
+    '-Wall',
+    `-lglfw`,
+    `-framework OpenGL`,
+    `-o ${output} ${input}`
+].join(' ');
 
 gulp.task('watch', (e)=>{
     gulp.watch(path.join(__dirname, '*.cpp'), ()=>{
-        exec(task, (err, stdout, stderr)=>{
+        exec(command, (err, stdout, stderr)=>{
             if ( err ){
                 console.error(err);
                 notifier.notify('build error');
